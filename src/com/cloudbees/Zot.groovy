4653505c7e9fb2b3e29f058bf495b5d870b5278b
Brent McConnell <bmcconnell@cloudbees.com>
2016-12-15 14:49:50 -0500

    Using https

--- src/com/cloudbees/Zot.groovy
+++ src/com/cloudbees/Zot.groovy
@@ -1,6 +1,6 @@
 // src/com/cloudbees/Zot.groovy
 package com.cloudbees;
 
-def checkOutFrom(repo) {
-  git url: "git@github.com:emcconne/${repo}"
+def checkOutFromGitHub(repo) {
+  git url: ${repo}"
 }
