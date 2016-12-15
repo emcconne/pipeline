9f8b9597f2e9522d1fe52fdbf04435af6256fcc4
Brent McConnell <bmcconnell@cloudbees.com>
2016-12-15 14:28:20 -0500

    Added githubapi

---
+++ src/com/cloudbees/githubapi.groovy
@@ -0,0 +1,21 @@
+package com.cloudbees;
+
+@Grab(group='org.kohsuke', module='github-api', version='1.80')
+import org.kohsuke.github.GitHub
+import org.kohsuke.github.GHRepository
+import com.cloudbees.groovy.cps.NonCPS
+
+class GitHubApi {
+	def githubapi
+
+	GitHubApi(user, oauth) {
+		GitHub github = GitHub.connect(user, oauth);
+		this.githubapi = github
+	}
+
+	def new_repo(name, description) {
+		GHRepository repo = this.github.createRepository(repo_name, description,
+  		"http://www.kohsuke.org/",true/*public*/);
+	}
+}
+
