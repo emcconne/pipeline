457ee9710a348cbb10398471e6d979cb9a8758b3
Brent McConnell <bmcconnell@cloudbees.com>
2016-12-15 15:56:43 -0500

    Fixed

--- src/com/cloudbees/GitHubApi.groovy
+++ src/com/cloudbees/GitHubApi.groovy
@@ -14,7 +14,7 @@ class GitHubApi {
 	}
 
 	def new_repo(name, description) {
-		GHRepository repo = this.githubapi.createRepository(repo_name, description,
+		GHRepository repo = this.githubapi.createRepository(name, description,
   		"http://www.kohsuke.org/",true/*public*/);
 	}
 }
